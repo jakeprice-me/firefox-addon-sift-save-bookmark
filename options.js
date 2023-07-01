943e32bf2ad70efe6b73b474bf29154b009166f6
Jake Price <git@jakeprice.dev>
2023-07-01 22:03:03 +0100

    Sort options screen out

--- options.js
+++ options.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var serverURLInput = document.getElementById("serverURL");
   var saveButton = document.getElementById("saveButton");
+  var messageContainer = document.getElementById("messageContainer");
 
   // Retrieve the server URL from the extension's storage
   chrome.storage.local.get("serverURL", function (data) {
@@ -13,9 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
   saveButton.addEventListener("click", function () {
     var serverURL = serverURLInput.value.trim();
 
+    if (!isValidURL(serverURL)) {
+      showMessage("Invalid URL. Please enter a valid URL starting with http:// or https://", "error");
+      return;
+    }
+
     // Store the server URL in the extension's storage
     chrome.storage.local.set({ serverURL: serverURL }, function () {
       console.log("Server URL saved:", serverURL);
+      showMessage("URL saved successfully", "message");
     });
   });
+
+  function isValidURL(url) {
+    var pattern = /^(https?:\/\/)/i;
+    return pattern.test(url);
+  }
+
+  function showMessage(message, className) {
+    messageContainer.innerHTML = '<div class="' + className + '">' + message + '</div>';
+  }
 });
+
