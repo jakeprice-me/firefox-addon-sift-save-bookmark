448070c24358e0e2f731858bed536e9130595a41
Jake Price <git@jakeprice.dev>
2023-07-07 22:28:40 +0100

    Add extra inputs and external archive option

--- popup.js
+++ popup.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var saveForm = document.getElementById("saveForm");
     var titleInput = document.getElementById("title");
     var urlInput = document.getElementById("url");
+    var typeInput = document.getElementById("type");
+    var categoryInput = document.getElementById("category");
+    var seriesInput = document.getElementById("series");
     var descriptionInput = document.getElementById("description");
     var tagsInput = document.getElementById("tags");
-    var archiveCheckbox = document.getElementById("archive");
+    var local_archiveCheckbox = document.getElementById("local_archive");
+    var external_archiveCheckbox = document.getElementById("external_archive");
 
     // Get the current active tab's information
     chrome.tabs.query({
@@ -39,23 +43,27 @@ document.addEventListener("DOMContentLoaded", function() {
     saveForm.addEventListener("submit", function(event) {
         event.preventDefault();
         var title = titleInput.value;
+        var type = typeInput.value;
+        var category = categoryInput.value;
+        var series = seriesInput.value;
         var url = urlInput.value;
         var tags = tagsInput.value || "";
         var description = descriptionInput.value || "";
-        var archive = archiveCheckbox.checked;
+        var local_archive = local_archiveCheckbox.checked;
+        var external_archive = external_archiveCheckbox.checked;
 
         // Retrieve the server URL from the extension's settings
         chrome.storage.local.get("serverURL", function(data) {
             var serverURL = data.serverURL;
             if (serverURL) {
-                savePageInfo(serverURL, title, url, description, tags, archive);
+                savePageInfo(serverURL, title, url, type, category, series, description, tags, local_archive, external_archive);
             } else {
                 showError("SIFT Server URL is not defined.");
             }
         });
     });
 
-    function savePageInfo(serverURL, title, url, description, tags, archive) {
+    function savePageInfo(serverURL, title, url, type, category, series, description, tags, local_archive, external_archive) {
         // Create an HTTP request to send the data to the server
         var xhr = new XMLHttpRequest();
         xhr.open("POST", serverURL + "/api/v1/save-bookmark", true);
@@ -65,9 +73,13 @@ document.addEventListener("DOMContentLoaded", function() {
         var bookmarkData = {
             title: title,
             url: url,
+            type: type,
+            category: category,
+            series: series,
             description: description,
             tags: tags,
-            archive: archive ? true : false // Include the "archive" value in the bookmarkData object as true or false based on the checkbox state
+            local_archive: local_archive ? true : false, // Include the "local_archive" value in the bookmarkData object as true or false based on the checkbox state
+            external_archive: external_archive ? true : false, // Include the "local_archive" value in the bookmarkData object as true or false based on the checkbox state
         };
 
         // Send the bookmarkData object as JSON data in the request body
